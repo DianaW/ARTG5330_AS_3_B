618b5accfeda47be88f0dec2e2710f85b106156f
JIN WANG <jinwang@JINdeMacBook-Pro.local>
2015-03-26 17:14:12 -0400

    what i did

--- script/script.js
+++ script/script.js
@@ -29,7 +29,11 @@ var yVariable = "CO2 emissions (kt)",
 //d3.map for metadata
 var metaDataMap = d3.map();
 
-//TODO: create a layout function for a treemap
+var colorScale=d3.scale.ordinal()
+.domain(["South Asia","Europe & Central Asia","Middle East & North Africa","East Asia & Pacific","Sub-Saharan Africa","Latin America & Caribbean","North America"])
+.range(["orange","blue","green","yellow","purple","grey","pink"]);
+
+//TODO: create a layout function for a tree map
 var treemap = d3.layout.treemap()
     .children(function(d){
         return d.values;
@@ -39,7 +43,7 @@ var treemap = d3.layout.treemap()
     })
     .size([width,height])
     .sticky(true)
-    .padding([5,5,5,5]);
+    .padding([2,2,2,2]);
 
 
 
@@ -53,18 +57,74 @@ function dataLoaded(err, rows, metadata){
 
     //First, combine "rows" and "metadata", so that each country is assigned to a region
 
+    rows.forEach(function(row){
+    row.region=metaDataMap.get(row.key);
+    });
+
     //Then create hierarchy based on regions, using d3.nest()
 
-    //Finally, perform a treemap layout on the data
+   var data=d3.nest()
+   .key(function(d){
+   return d.region;
+   })
+   .entries(rows);
 
-    //draw(root);
+    //Finally, perform a tree map layout on the data
+
+   var root={
+   key:"regions",
+   values:data
+   };
+    draw(root);
 }
 
 function draw(root){
     //Append <rect> element for each node in the treemap
-
     //Also append <text> label for each tree node that is a leaf
+var nodes = svg.selectAll('.node')
+        .data(treemap(root), function(d){return d.key;});
+
+    var nodesEnter = nodes
+        .enter()
+        .append('g')
+        .attr('class',"node")
+        .classed('leaf',function(d){
+            return !(d.children);
+        })
+        .attr('transform',function(d){
+            return "translate("+d.x+','+d.y+')';
+        });
+    nodesEnter
+        .append('rect')
+        .attr('width',function(d){return d.dx; })
+        .attr('height',function(d){return d.dy;})
+        .style('fill-opacity',function(d){ return 1-d.depth/8})
+        .style('fill',function(d){
+        return colorScale(metaDataMap.get(d.key));
+        });
+    nodesEnter
+        .each(function(d){
+            if(d.dx > 60){
+                d3.select(this).append('text')
+                    .text(d.key)
+                    .attr('dx', d.dx/2)
+                    .attr('dy', d.dy/2)
+                    .attr('text-anchor','middle');
+            }
+           /*if(300>d.dx > 60){
+                           d3.select(this).append('text')
+                               .text(d.key)
+                               .attr('dx', d.dx/2)
+                               .attr('dy', d.dy/2)
+                               .attr('text-anchor','left');
+                       }*/
+        });
+
 }
+
+
+
+
 
 function parse(d){
     var newRow = {
@@ -75,8 +135,7 @@ function parse(d){
     for(var i=1990; i<=2013; i++){
         var heading = i + " [YR" + i + "]";
         newRow.data.set(
-            i,
-            (d[heading]=="..")?0:+d[heading]
+            i,(d[heading]=="..")?0:+d[heading]
         );
     }
 
